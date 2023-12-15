2445312411e61ce171ad1a05a4002ab70e5436dc
Sofia <36406321+SofjaIRM@users.noreply.github.com>
2023-12-15 16:18:41 +0000

    Move variables to object

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const config = {
-  followers: {
-    hash: 'c76146de99bb02f6415203be841dd25a',
-    path: 'edge_followed_by'
+const CONFIG = {
+  FOLLOWERS: {
+    HASH: 'c76146de99bb02f6415203be841dd25a',
+    PATH: 'edge_followed_by'
   },
-  following: {
-    hash: '3dec7e2c57367ef3da3d987d89f9dbc8',
-    path: 'edge_follow'
+  FOLLOWING: {
+    HASH: '3dec7e2c57367ef3da3d987d89f9dbc8',
+    PATH: 'edge_follow'
   }
 };
 
@@ -21,28 +21,30 @@ function sleep(ms) {
   })
 }
 function afterUrlGenerator(hash, nextCode) {
-  return `https://www.instagram.com/graphql/query/?query_hash=${hash}&variables={"id":"${ds_user_id}","include_reel":"true","fetch_mutual":"false","first":"24","after":"${nextCode}"}`
+  return `https://www.instagram.com/graphql/query/?query_hash=${hash}&variables={"id":"${initialData.dsUserId}","include_reel":"true","fetch_mutual":"false","first":"24","after":"${nextCode}"}`
 }
 
-let followedPeopleCount;
+const initialData = {
+  dsUserId: getCookie("ds_user_id"),
+  followedPeopleCount: null,
+  totalUnfollowers: [],
+  totalFollowing: [],
+  totalFollowers: [],
+  unfollowCounter: 0,
+  scrollCicle: 0,
+  doNext: true,
+  initialURL: null,
+}
+
 let type;
 let array;
-let ds_user_id = getCookie("ds_user_id");
-let initialURL = null;
-let doNext = true;
-let allUnfollowers = [];
-let allFollowing = []
-let allFollowers = [];
-let getUnfollowCounter = 0;
-let scrollCicle = 0;
-
 
 let scriptSelected = prompt("Which script do you want to run?", "followers");
 
 if (scriptSelected !== "followers") {
-  await startScript(config.following);
+  await startScript(CONFIG.FOLLOWING);
 } else {
-  await startScript(config.followers);
+  await startScript(CONFIG.FOLLOWERS);
 }
 
 async function saveFile(type, array) {
@@ -56,66 +58,66 @@ async function saveFile(type, array) {
 }
 
 async function startScript(config) {
-  const { hash, path } = config;
-  const isPathFollowing = path === 'edge_follow';
-  const isPathFollowers = path === 'edge_followed_by';
+  const { HASH, PATH } = config;
+  const isPathFollowing = PATH === 'edge_follow';
+  const isPathFollowers = PATH === 'edge_followed_by';
 
-  if(!initialURL) {
-    initialURL = `https://www.instagram.com/graphql/query/?query_hash=${hash}&variables={"id":"${ds_user_id}","include_reel":"true","fetch_mutual":"false","first":"24"}`;
+  if(!initialData.initialURL) {
+    initialData.initialURL = `https://www.instagram.com/graphql/query/?query_hash=${HASH}&variables={"id":"${initialData.dsUserId}","include_reel":"true","fetch_mutual":"false","first":"24"}`; //?
   }
-  while (doNext) {
+  while (initialData.doNext) {
     let receivedData;
     try {
-      receivedData = await fetch(initialURL).then(res => res.json());
+      receivedData = await fetch(initialData.initialURL).then(res => res.json());
     } catch (e) {
       continue;
     }
 
-    followedPeopleCount || (followedPeopleCount = receivedData.data.user[path].count);
+    initialData.followedPeopleCount || (initialData.followedPeopleCount = receivedData.data.user[PATH].count);
 
-    doNext = receivedData.data.user[path].page_info.has_next_page;
-    initialURL = afterUrlGenerator(hash, receivedData.data.user[path].page_info.end_cursor);
-    getUnfollowCounter += receivedData.data.user[path].edges.length;
+    initialData.doNext = receivedData.data.user[PATH].page_info.has_next_page;
+    initialData.initialURL = afterUrlGenerator(HASH, receivedData.data.user[PATH].page_info.end_cursor);
+    initialData.unfollowCounter += receivedData.data.user[PATH].edges.length;
 
-    receivedData.data.user[path].edges.forEach(x => {
+    receivedData.data.user[PATH].edges.forEach(x => {
       if (isPathFollowing) {
         const isUnfollower = !x.node.follows_viewer;
         if (isUnfollower) {
-          allUnfollowers.push(x.node);
+          initialData.totalUnfollowers.push(x.node);
         }
 
-        allFollowing.push(x.node);
+        initialData.totalFollowing.push(x.node);
       }
       if (isPathFollowers) {
-        allFollowers.push(x.node);
+        initialData.totalFollowers.push(x.node);
       }
     })
 
-    console.log(`%c Progress ${getUnfollowCounter}/${followedPeopleCount} (${parseInt((getUnfollowCounter/followedPeopleCount)*100)}%)`, 'background: #222; color: #bada55;font-size: 35px;');
+    console.log(`%c Progress ${initialData.unfollowCounter}/${initialData.followedPeopleCount} (${parseInt((initialData.unfollowCounter/initialData.followedPeopleCount)*100)}%)`, 'background: #222; color: #bada55;font-size: 35px;');
 
     await sleep(Math.floor(400 * Math.random()) + 1000);
 
-    scrollCicle++;
+    initialData.scrollCicle++;
 
-    if (scrollCicle > 6){
-      scrollCicle = 0;
+    if (initialData.scrollCicle > 6){
+      initialData.scrollCicle = 0;
       console.log(`%c Sleeping 10 segs to prevent getting temp blocked`, 'background: #222; color: ##FF0000;font-size: 35px;');
       await sleep(10000);
     }
   }
 
   if (isPathFollowing) {
-    console.log(`%c ${allUnfollowers.length} users don't follow you`, 'background: #222; color: #bada55;font-size: 25px;');
-    console.log(`%c ${allFollowing.length} users you are following`, 'background: #222; color: #bada55;font-size: 25px;');
+    console.log(`%c ${initialData.totalUnfollowers.length} users don't follow you`, 'background: #222; color: #bada55;font-size: 25px;');
+    console.log(`%c ${initialData.totalFollowing.length} users you are following`, 'background: #222; color: #bada55;font-size: 25px;');
     type = 'following';
-    array = allFollowing;
+    array = initialData.totalFollowing;
 
   }
 
   if (isPathFollowers) {
-    console.log(`%c ${allFollowers.length} users don't follow you`, 'background: #222; color: #bada55;font-size: 25px;');
+    console.log(`%c ${initialData.totalFollowers.length} users don't follow you`, 'background: #222; color: #bada55;font-size: 25px;');
     type = 'followers';
-    array = allFollowers;
+    array = initialData.totalFollowers;
   }
 
   console.log(`%c All DONE!`, 'background: #222; color: #bada55;font-size: 25px;');
