8f2e7bc30b47e6f125bf4b3258e1f6fbdffd5d81
Sofia <36406321+SofjaIRM@users.noreply.github.com>
2024-01-18 22:23:23 +0000

    Refactor user interaction code to src/index.js
    The user interaction handling code has been moved from the main index.js file into src/index.js. The code to ask the user for the function they wish to run has been wrapped into a `askQuestion` function. In addition, the handling of errors has been improved to ensure that the correct information is displayed and the user is asked the question again in the event of an error.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
+const readline = require('readline');
 const { startFollowersStatistics } = require('./followers');
 const { startFollowingStatistics } = require('./following');
 
-module.exports = {
-  startFollowersStatistics,
-  startFollowingStatistics,
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askQuestion = () => {
+  rl.question("\nWhich function do you want to execute? (Select one of the following options)\n(1 - startFollowersStatistics, 2 - startFollowingStatistics, exit - stop running code)\n", async (answer) => {
+    console.log(`\nOption Selected: ${answer}`);
+
+    switch (answer.trim()) {
+      case '1':
+        try {
+          console.log('Running startFollowersStatistics...');
+          await startFollowersStatistics();
+          console.log('startFollowersStatistics finished successfully');
+          rl.close();
+          process.stdin.end();
+        } catch (error) {
+          console.error('Error running startFollowersStatistics: ', error.message);
+          askQuestion();
+        }
+        break;
+      case '2':
+        try {
+          console.log('Running startFollowingStatistics...');
+          await startFollowingStatistics();
+          console.log('startFollowingStatistics finished successfully');
+          rl.close();
+          process.stdin.end();
+        } catch (error) {
+          console.error('Error running startFollowingStatistics: ', error.message);
+          askQuestion();
+        }
+        break;
+      default:
+        console.error('\x1b[31mInvalid selection! Select a valid option!\x1b[0m');
+        askQuestion(); // repete a pergunta
+        break;
+    }
+  });
 }
+
+askQuestion();
